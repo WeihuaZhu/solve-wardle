32516f6091052f8c0e8fa1b01a961e938c2c9f1d
Weihua Zhu <zhuweihua2746@gmail.com>
2022-01-14 00:12:08 -0800

    some observations on candidate_0

--- stats.py
+++ stats.py
@@ -13,7 +13,23 @@ def get_single_letter_freq_wo_dup():
     return count
 
 
+def count_occurence(letters, word):
+    occurence = 0
+    word_set = set(word)
+    for letter in letters:
+        if letter in word_set:
+            occurence += 1
+    return occurence
+
+
 count = get_single_letter_freq_wo_dup()
 count_tuples = [(k, v) for k, v in count.items()]
 count_tuples.sort(key=lambda x: x[1], reverse=True)
 print(count_tuples)
+candidate_letters = [count_tuple[0] for count_tuple in count_tuples[:5]]
+
+candidate_0s = []
+for word in ALL_WORDS:
+    if count_occurence(candidate_letters, word) >= 4:
+        candidate_0s.append(word)
+print(candidate_0s)
